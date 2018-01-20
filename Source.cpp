8c249256f720e42e7c63f698d50cf6ad18f3d195
ampdolinski <34243904+ampdolinski@users.noreply.github.com>
2018-01-20 01:28:23 +0100

    Program z pętlą i referencją
    - pętla zerowanie,
    - jeden argument referencyjny,
    - jedna pętla do powtarzania/zamykania programu, ale przyjmie wszystko... :(

---
+++ Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+//**************************************************************
+//definicja nazwy i elementów funkcji wewnêtrznej
+void zerowanie(int wart, int &druga_wart)
+{
+	cout << "\tW funkcji zerowanie przed zerowaniem \n"
+		<< "\twart = " << wart << ", a druga_wart = " << druga_wart << ".\n";
+
+	wart = 0;
+	druga_wart = 0;
+
+	cout << "\tW funkcji zerowanie po zerowaniu \n"
+		<< "\twart = " << wart << ", a druga_wart = " << druga_wart << ".\n";
+
+}
+//**************************************************************
+int main()
+{
+	char znak; int a = 55, b = 63;
+	
+	do
+	{
+
+		cout << "Przed wywolaniem funkcji: zerowanie \n"
+			<< "a = " << a << ", a b = " << b << ".\n";
+
+		zerowanie(a, b);
+
+		cout << "Po wywolaniu funkcji: zerowanie \n"
+			<< "a = " << a << ", a b = " << b << ".\n";
+
+		//a teraz pytanie o zamkniêcie czy powtórzenie programu
+
+		cout << "\nCzy chcesz zakonczyc prace programu? [t/n]?\n";
+		cin >> znak;
+		cout << "\n";
+
+	}
+
+	while (znak != 't');
+	return 0;
+}
